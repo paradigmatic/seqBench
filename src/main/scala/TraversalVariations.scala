8a9a05e6ef6e4da83b5e89129fa54ada48ef362d
JL Falcone <jean-luc.falcone@unige.ch>
2012-02-27 09:27:22 +0100

    Better implementations

--- src/main/scala/TraversalVariations.scala
+++ src/main/scala/TraversalVariations.scala
@@ -3,14 +3,13 @@ package org.example
 import annotation.tailrec
 import com.google.caliper.Param
 
-
-class TraversalVariations extends SimpleScalaBenchmark {
-  
-  @Param(Array("1", "10", "100", "1000" ))
-  val length: Int = 0
+object WordsGenerator {
 
   val wordLength = 8 
-  val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".map(_.toString)
+  val big = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  val small = big.toLowerCase
+  val letters = (big+small).map(_.toString)
+
   val rng = new util.Random
 
   def randomWord =  {
@@ -21,61 +20,60 @@ class TraversalVariations extends SimpleScalaBenchmark {
     str
   }
   
-  
-  val randomStream = {
+  lazy val stream = {
     def drawWord(): Stream[String] =
       Stream.cons( randomWord, drawWord() )
     drawWord()
   }
+}
+
+
+class TraversalVariations extends SimpleScalaBenchmark {
+  
+  @Param(Array("10" ))
+  val length: Int = 0
 
   var words: List[String] = _
   
   override def setUp() {
-    words = randomStream.take(length).toList
+    words = WordsGenerator.stream.take(length).toList
   }
 
   def isCapitalized( s: String ) =
     java.lang.Character.isUpperCase(s.charAt(0))
-  
-  def timeFunctional(reps: Int) = repeat(reps) {
-    val (wlengthsMapUnzip, wcapsMapUnzip) =
-      words.map(word => (word.length, word(0).isUpper)).unzip
-    (wlengthsMapUnzip.head,wcapsMapUnzip.head) 
+
+ 
+ def timeFunctional(reps: Int) = repeat(reps) {
+   val wLength = words.map( _.length )
+   val wCaps = words.map( isCapitalized )
+    (wLength.head, wCaps.head) 
   }
 
+ 
+
   def timeReassign(reps: Int) = repeat(reps) {
-    var wlengthsReassign = List[Int]()
-    var wcapsReassign = List[Boolean]()
-    words.foreach { word =>
-      wlengthsReassign = word.length :: wlengthsReassign
-		   wcapsReassign = isCapitalized(word) :: wcapsReassign
-		 }
-    (wlengthsReassign.head, wcapsReassign.head)
+    var wLength = List[Int]()
+    var wCaps = List[Boolean]()
+    for( word <- words ) {
+      wLength ::= word.length
+      wCaps ::= isCapitalized(word)
+    }
+    (wLength.head, wCaps.head)
   }
+
+
 
  def timeBuffer(reps: Int) = repeat(reps) {
    import collection.mutable._
-   val wlengthsBuffer = ArrayBuffer[Int]()
-   val wcapsBuffer = ArrayBuffer[Boolean]()
-   words.foreach { word =>
-     wlengthsBuffer.append(word.length)
-		  wcapsBuffer.append(isCapitalized(word))
-		}
-   ( wlengthsBuffer.head, wcapsBuffer.head )
+   val wLength = new ArrayBuffer[Int]()
+   val wCaps = new ArrayBuffer[Boolean]()
+   for( word <- words ) {
+     wLength.append(word.length)
+     wCaps.append(isCapitalized(word))
+   }
+   ( wLength.head, wCaps.head )
  }
 
-  def timeFixedSizeArray(reps: Int) = repeat(reps) {
-
-    val wlengthsArray2 = Array.fill(words.length)(0)
-    val wcapsArray2 = Array.fill(words.length)(false)
-    var index = 0
-    words.foreach { word =>
-      wlengthsArray2(index) = word.length
-		   wcapsArray2(index) = isCapitalized(word)
-		   index += 1
-		 }
-    ( wlengthsArray2.head, wcapsArray2.head )
-  }
 
   def timeOldSchool(reps: Int) = repeat(reps) {
     val n = words.length
@@ -93,27 +91,21 @@ class TraversalVariations extends SimpleScalaBenchmark {
   }
 
 
-  def timeRecurs(reps: Int) = repeat(reps) {
-    def lengthCapRecurWrap(inputWords: List[String]): (List[Int], List[Boolean]) = {
-      
-      // This function is hidden from code that doesn't
-      def lengthCapRecurHelp(
-	inputWords: List[String],
-	lengths: List[Int],
-	caps: List[Boolean]): (List[Int], List[Boolean]) = inputWords match {
-	case Nil =>
-	  (lengths, caps)
-	  case head :: tail =>
-	    lengthCapRecurHelp(tail, head.length :: lengths, isCapitalized(head) :: caps)
-      }
-      val (l,c) = lengthCapRecurHelp(words, List[Int](), List[Boolean]())
-      (l.reverse, c.reverse)
-    }
-    val (wlengthsRecurWrap, wcapsRecurWrap) = lengthCapRecurWrap(words)
-    (wlengthsRecurWrap.head, wcapsRecurWrap.head)
-  }
+ def timeRecurs(reps: Int) = repeat(reps) {
 
-  
+   @tailrec
+   def lengthAndCaps( ws: List[String], ls: List[Int], cs: List[Boolean] ): (List[Int],List[Boolean]) = 
+     if( ws.isEmpty ) (ls.reverse, cs.reverse)
+     else {
+       val w = ws.head
+       lengthAndCaps( ws.tail, w.length::ls, isCapitalized(w)::cs )
+     }
+
+   val (wLength,wCaps) = lengthAndCaps( words, Nil, Nil )
+   ( wLength.head, wCaps.head )
+ }
+
+ 
   
   override def tearDown() {
     // clean up after yourself if required
